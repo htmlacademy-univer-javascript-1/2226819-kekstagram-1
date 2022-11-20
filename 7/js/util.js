cf36a1d8e64f81a4711a5bfff19e8d3595ce24cb
Keks <keks@htmlacademy.ru>
2022-11-20 15:57:38 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/util.js
@@ -0,0 +1,21 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
+const getRandomIntInclusive = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
+// Функция для проверки длины введённого комментария
+const isCorrectLength = (comment, maxLength) => {
+  return comment.length <= maxLength;
+}
+
+// Функция для получения рандомного элемента из массива elements
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomIntInclusive(0, elements.length - 1)];
+};
+
+
+export {getRandomArrayElement};
